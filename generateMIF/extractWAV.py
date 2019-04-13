083c3e807160d36cc0c31cdd0a0bca18def22532
Taian <taianmeca@gmail.com>
2019-04-12 22:57:16 -0300

    ajeitada no gerador de mif com downsampling

--- generateMIF/extractWAV.py
+++ generateMIF/extractWAV.py
@@ -1,23 +1,35 @@
-import wave, struct
-DEPTH = 8192
+import librosa
+
+
+def tohex(val, nbits):
+    return hex((val + (1 << nbits)) % (1 << nbits))
+
+
+# Downsample 9600 Hz
+y1, sr = librosa.load('smw_1_up.wav', sr=9600)
+y2, sr = librosa.load('smw_bubble_pop.wav', sr=9600)
+y3, sr = librosa.load('smw_coin.wav', sr=9600)
+y4, sr = librosa.load('smw_fireball.wav', sr=9600)
+y5, sr = librosa.load('smw_jump.wav', sr=9600)
+
+# lista de valores em float
+data1 = y1.tolist()
+data2 = y2.tolist()
+data3 = y3.tolist()
+data4 = y4.tolist()
+data5 = y5.tolist()
+datat = [len(data1)] + data1 + [len(data2)] + data2 + [len(data3)] + data3 + [len(data4)] + data4 + [len(data5)] + data5
+
+# normalizando com inteiros em 16 bits
+datanew = map(lambda x: int(x / max(abs(min(datat)), max(datat)) * 32767), datat)
+
+# Mif headers
+DEPTH = 1024 * 1024
 WIDTH = 16
 ADDRESS_RADIX = 'HEX'
 DATA_RADIX = 'HEX'
 
-def tohex(val, nbits):
-  return hex((val + (1 << nbits)) % (1 << nbits))
-
-
-
-
-w = wave.open('smb3_sound_effects_1_up.wav', 'r')
-# framerate: numero de sample por segundo
-# nframes: numero de sample (informação da amplitude)
-# Returns a tuple (nchannels, sampwidth, framerate, nframes, comptype, compname)
-print(w.getparams())
-
-txsamprate = w.getparams().framerate/9600
-
+# Mif file
 f = open("initialization.mif", "w+")
 f.write("DEPTH = %d;" % DEPTH)
 f.write('\t\t\t' + '% Memory depth and width are required %' + '\n')
@@ -34,11 +46,18 @@ f.write("-- Specify values for addresses, which can be single address or range" 
 f.write("CONTENT" + '\n')
 f.write("BEGIN" + '\n')
 
-for i in range(44, w.getnframes()):
-    waveData = w.readframes(1)
-    # print(len(waveData))
-    data = struct.unpack("<h", waveData)
-    f.write(hex(i-44) + '\t')
-    f.write(":\t" + tohex(data[0], 16) + ";" + "\n")
+# indexes
+f.write(hex(0) + "\t:\t" + hex(32) + ";\n")
+f.write(hex(1) + "\t:\t" + hex(32 + len(data1)) + ";\n")
+f.write(hex(2) + "\t:\t" + hex(32 + len(data1) + len(data2)) + ";\n")
+f.write(hex(3) + "\t:\t" + hex(32 + len(data1) + len(data2) + len(data3)) + ";\n")
+f.write(hex(4) + "\t:\t" + hex(32 + len(data1) + len(data2) + len(data3) + len(data4)) + ";\n")
+
+for i in range(5, 32):
+    f.write(hex(i) + "\t:\t" + hex(0) + ";\n")
+
+# first wave
+for i in range(32, len(datanew)):
+    f.write(hex(i) + "\t:\t" + hex(datanew[i - 32]) + ";\n")
 
 f.write("END;")
