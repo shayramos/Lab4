7d5da6813210ef048d1857e3bbde6147e78f2f19
Shay Ramos <shayramos@hotmail.com>
2019-07-07 16:01:39 -0300

    Interface de áudio

--- audioProcessor/InterfaceAvalon/InterfaceAvalon_inst.v
+++
@@ -1,7 +0,0 @@
-	InterfaceAvalon u0 (
-		.audioprocessor_0_conduit_end_bclk            (<connected-to-audioprocessor_0_conduit_end_bclk>),            // audioprocessor_0_conduit_end.bclk
-		.audioprocessor_0_conduit_end_codecserialdata (<connected-to-audioprocessor_0_conduit_end_codecserialdata>), //                             .codecserialdata
-		.audioprocessor_0_conduit_end_lrck            (<connected-to-audioprocessor_0_conduit_end_lrck>),            //                             .lrck
-		.clk_clk                                      (<connected-to-clk_clk>)                                       //                          clk.clk
-	);
-
--- niosAudio/InterfaceAvalon/InterfaceAvalon_inst.v
+++
@@ -1,7 +0,0 @@
-	InterfaceAvalon u0 (
-		.codecdata_bclk            (<connected-to-codecdata_bclk>),            // codecdata.bclk
-		.codecdata_codecserialdata (<connected-to-codecdata_codecserialdata>), //          .codecserialdata
-		.codecdata_lrck            (<connected-to-codecdata_lrck>),            //          .lrck
-		.clk_clk                   (<connected-to-clk_clk>)                    //       clk.clk
-	);
-
